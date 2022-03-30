fb20345de9c5fd3a9854d8e37b2415a48b737e82
Muhammad Thariq Hidayat <muhammadthariqhs@gmail.com>
2022-03-31 01:09:34 +0700

    fourth step

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ numbers.forEach((number) => {
         updateScreen(event.target.value)
     })  
 })
+
+let prevNumber = ""
+let calculationOperator = ""
+let currentNumber = "0"
+
+const inputNumber = (number) => {
+    if (currentNumber === "0") {
+        currentNumber = number
+    } else {
+        currentNumber += number
+    }
+}
+
+numbers.forEach((number) => {
+    number.addEventListener("click", (event) => {
+        inputNumber(event.target.value)
+        updateScreen(currentNumber)
+    })
+})
+
+const operators = document.querySelectorAll(".operator")
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+       inputOperator(event.target.value)
+    })
+})
+
+const inputOperator = (operator) => {
+    prevNumber = currentNumber
+    calculationOperator = operator
+    currentNumber = ""
+}
